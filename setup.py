a1d497026680ae5d507470531c8d7e904c371e77
Johansson Johannes <a002028@ad.smhi.se>
2021-09-22 11:18:54 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ README = open('READMEpypi.rst', 'r').read()
 
 setuptools.setup(
     name=NAME,
-    version="0.1.12",
+    version="0.2.0",
     author="SMHI - NODC",
     author_email="shark@smhi.se",
     description="Validate data delivery at the Swedish NODC",
